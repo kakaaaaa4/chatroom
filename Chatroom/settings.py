bab7f3b7cebd34fec45ea45dbd31da8c16520b48
kakaaaaa4 <kakaaaaa4@163.com>
2022-10-15 11:29:33 +0800

    db settings change

--- Chatroom/settings.py
+++ Chatroom/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
      'ENGINE': 'django.db.backends.mysql',
      'NAME': 'djangoweb',
      'USER': 'root',
-     'PASSWORD': '450163bhy',
+     'PASSWORD': '123456',
      'HOST': '127.0.0.1',
      'PORT': 3306,
      }
